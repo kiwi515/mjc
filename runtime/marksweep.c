d9afc92389eca8b00e8740999d55219ba60d9ba8
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2023-10-31 13:09:15 -0400

    Empty files for mark-sweep

---
+++ runtime/marksweep.c
@@ -0,0 +1,10 @@
+/*
+ * Author:  Trevor Schiff, tschiff2020@my.fit.edu
+ * Course:  CSE 4251, Section 01, Spring 2023
+ * Project: MiniJava Compiler Project
+ * Charset: US-ASCII
+ */
+
+#include "marksweep.h"
+#include <assert.h>
+#include <stdio.h>
