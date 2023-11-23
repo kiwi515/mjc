5a84bf522d1130cd35832d78e37676729c12a934
tygutowski <tygutowski@gmail.com>
2023-11-23 15:56:33 -0500

    added basic mark and sweep algorithms

--- runtime/marksweep.c
+++ runtime/marksweep.c
@@ -1,10 +1,69 @@
 /*
- * Author:  Trevor Schiff, tschiff2020@my.fit.edu
- * Course:  CSE 4251, Section 01, Spring 2023
- * Project: MiniJava Compiler Project
- * Charset: US-ASCII
+ * Author:  Tyler Gutowski
  */
 
 #include "marksweep.h"
+#include "heap.h"
+#include <stddef.h>
 #include <assert.h>
-#include <stdio.h>
+#include <stdbool.h>
+
+static void mark(HeapHeader* header);
+static void sweep();
+
+void gc_mark() {
+    void* current_block = heap_list_head; 
+
+    while (current_block != NULL) {
+        // valid pointer
+        if (heap_is_header(current_block)) {
+            HeapHeader* header = heap_get_header(current_block);
+            // if its being used
+            if (header->ref > 0) {
+                mark(header);
+            }
+            current_block = (void*)(header->next);
+        // if its not valid
+        } else {
+            break;
+        }
+    }
+}
+
+static void mark(HeapHeader* header) {
+    assert(header != NULL);
+    // if already marked
+    if (header->marked) {
+        return;
+    }
+    // mark object
+    header->marked = true;
+}
+
+void gc_sweep() {
+    sweep();
+}
+
+static void sweep() {
+    HeapHeader* current = heap_get_header(heap_list_head);
+    HeapHeader* temp;
+    // for each object in the pointer linked list
+    while (current != NULL) {
+        // if the object is not marked
+        if (!current->marked) {
+            // free the object
+            temp = heap_get_header(current->next);
+            heap_free(current->data);
+            current = temp;
+        // if the object is marked
+        } else {
+            // dont free
+            current = heap_get_header(current->next);
+        }
+    }
+}
+
+void gc_collect() {
+    gc_mark();
+    gc_sweep();
+}
--- runtime/marksweep.h
+++ runtime/marksweep.h
@@ -1,14 +1,12 @@
 /*
- * Author:  Trevor Schiff, tschiff2020@my.fit.edu
- * Course:  CSE 4251, Section 01, Spring 2023
- * Project: MiniJava Compiler Project
- * Charset: US-ASCII
+ * Author:  Tyler Gutowski
  */
 
 #ifndef MINI_JAVA_COMPILER_MARKSWEEP_H
 #define MINI_JAVA_COMPILER_MARKSWEEP_H
-#include "heap.h"
-#include "types.h"
-#include <stddef.h>
 
-#endif
+void gc_mark();
+void gc_sweep();
+void gc_collect();
+
+#endif // MINI_JAVA_COMPILER_MARKSWEEP_H
